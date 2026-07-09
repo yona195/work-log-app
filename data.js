db5b158d2911eb0f75755c3012b70ea928c91a42
yona195 <yona195@gmail.com>
2026-07-09 21:13:00 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,24 @@
+const STORAGE_KEY = "work_log_app_data";
 
+let appData = {
+  employees: [],
+  sites: [],
+  buildings: [],
+  customers: [],
+  workLogs: []
+};
+
+function saveData() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(appData));
+}
+
+function loadData() {
+  const saved = localStorage.getItem(STORAGE_KEY);
+  if (saved) {
+    appData = JSON.parse(saved);
+  }
+}
+
+function generateId() {
+  return Date.now().toString();
+}
